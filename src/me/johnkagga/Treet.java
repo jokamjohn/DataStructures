158a91d4c1efb7677e2912496bd7c5a79f047ae1
jokamjohn <johnkagga@gmail.com>
2015-06-20 09:53:25 +0300

    Treet class

--- src/me/johnkagga/Treet.java
+++ src/me/johnkagga/Treet.java
@@ -1,7 +1,18 @@
 package me.johnkagga;
 
+import java.util.Date;
+
 /**
  * Created by JohnKagga on 6/20/2015.
  */
 public class Treet {
+    private String mAuthor;
+    private String mDescription;
+    private Date mDate;
+
+    public Treet(String author, String description, Date date) {
+        mAuthor = author;
+        mDescription = description;
+        mDate = date;
+    }
 }
